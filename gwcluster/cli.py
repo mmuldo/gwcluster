aed05da731a9d8a5143c1f061b344c8ae40eea20
Matt Muldowney <matt.muldowney@gmail.com>
2022-07-19 23:53:37 -0700

    working seismometer command

--- gwcluster/cli.py
+++ gwcluster/cli.py
@@ -1,26 +1,123 @@
 import typer
 from seismometer import ClusteredSeismicData
-from gwpy.timeseries import TimeSeries, TimeSeriesDict
+from gwpy.timeseries import TimeSeriesDict
 from sklearn.cluster import KMeans
 from gwpy.plot import Plot
+import os
+import yaml
+from typing import Optional, Any
 
+CONFIG_FILE_PATH = f'{os.getenv("HOME")}/.gwcluster.yml'
+
+# the cli
 app = typer.Typer()
+
+# user config settings
+if os.path.exists(CONFIG_FILE_PATH):
+    with open(CONFIG_FILE_PATH, 'r') as config_file:
+        config = yaml.load(config_file, Loader=yaml.Loader)
+else:
+    config = None
+
+def default(
+    param: Optional[Any],
+    parent_key: str,
+    config_key: str
+) -> Any:
+    '''
+    defaults parameter to value in config if parameter is not set
+
+    Parameters
+    ----------
+    param : Any
+        variable that could None or could have a value
+    parent_key : str
+        key to dictionary with relevant configs (e.g. 'seismometer')
+    config_key : str
+        key under parent_key
+        
+    '''
+    if not param:
+        try:
+            param = config[parent_key][config_key]
+        except (TypeError, KeyError):
+            raise Exception(f'{parent_key}.{config_key} not found in config file at {CONFIG_FILE_PATH}')
+    return param
+
+def parse(arg: str) -> Any:
+    '''
+    tries to parse the value to an appropriate type, e.g. int if arg is an
+    integer
+    '''
+    try:
+        arg = int(arg)
+    except ValueError:
+        try:
+            arg = float(arg)
+        except ValueError:
+            pass
+
+    return arg
+
 
 @app.command()
 def seismometer(
-    ifo: str,
-    start: str,
-    end: str,
-    host: str = '',
-    port: int = 0
+    clustering: Optional[str] = None,
+    clustering_kwargs: Optional[str] = None,
+    ifo: Optional[str] = None,
+    start: Optional[str] = None,
+    end: Optional[str] = None,
+    host: Optional[str] = None,
+    port: Optional[int] = None,
 ):
-    dirs = ['X', 'Y', 'Z']
+    '''
+    performs clustering on seismometer data
 
+    Parameters
+    ----------
+    clustering : str, defaults to value in config
+        clustering algorithm to use
+    clustering_kwargs : str, defaults to value in config
+        kwargs to pass to clustering algorithm, formatted as "key1=value1,key2=value2,..."
+    ifo : str, defaults to value in config
+        ifo of channels to grab seismometer data from
+    start : str, default to value in config
+        start time (must be gps parsable)
+    end : str, default to value in config
+        end time (must be gps parsable)
+    host : str, default to value in config
+        host to grab channel data from
+    port : int, default to value in config
+        port on host
+    '''
+    # default parameters from config file
+    ifo = default(ifo, 'seismometer', 'ifo')
+    start = default(start, 'seismometer', 'start')
+    end = default(end, 'seismometer', 'end')
+    host = default(host, 'seismometer', 'host')
+    port = default(port, 'seismometer', 'port')
+
+    # default clustering algorithm
+    if clustering:
+        parsed_clustering_kwargs = {
+            pair.split('=')[0]: parse(pair.split('=')[1])
+            for pair in clustering_kwargs.split(',')
+        }
+    else:
+        clustering_from_config = config['seismometer']['clustering']
+        clustering, parsed_clustering_kwargs = next(iter(clustering_from_config.items()))
+
+
+    # directional movement of seismometer
+    directions = ['X', 'Y', 'Z']
+
+    # channels with raw data
     raw_channels = [
         f'{ifo}:PEM-SEIS_BS_{dir}_OUT_DQ'
-        for dir in dirs
+        for dir in directions
     ]
 
+    # channels with blrms data
     blrms_channels = [
         f'{ifo}:PEM-RMS_BS_{dir}_{band}.mean'
         for band in [
@@ -31,8 +128,10 @@ def seismometer(
             '3_10',
             '10_30',
         ]
-        for dir in dirs
+        for dir in directions
     ]
+
+    # other parameters to pass to TimeSeriesDict
     non_channel_params = {
         "host": host,
         "port": port,
@@ -42,19 +141,18 @@ def seismometer(
     }
 
     csd = ClusteredSeismicData(
-        raw=TimeSeriesDict.fetch(
+        raw=TimeSeriesDict.get(
             channels=raw_channels,
             **non_channel_params
         ),
-        blrms=TimeSeriesDict.fetch(
+        blrms=TimeSeriesDict.get(
             channels=blrms_channels,
             **non_channel_params
         ),
-        clusters=KMeans(n_clusters=5)
+        clustering=eval(f'{clustering}(**{parsed_clustering_kwargs})')
     )
 
-    csd.clusters.fit(csd.vectors())
-    for psds in csd.psds_of_centers():
+    for psds in csd.psds_of_centers(psd_length=600):
         plot = Plot(*psds, xscale='log', yscale='log')
         plot.show()
 
--- gwcluster/seismometer.py
+++ gwcluster/seismometer.py
@@ -1,78 +1,100 @@
-from astropy.units.quantity import Quantity
-from astropy.time import Time
-from datetime import datetime
-import numpy as np
-from dataclasses import dataclass
-from gwpy.timeseries import TimeSeries, TimeSeriesDict
-from gwpy.frequencyseries import FrequencySeries
-from gwpy.types.index import Index
-from gwpy.time import from_gps
-from typing import Union
+'''
+for clustering time series data from seismometers
+'''
+
 from sklearn.cluster import KMeans, DBSCAN
 
-GPSParsable = Union[float, datetime, Time, str]
-SupportedClusterAlgorithms = Union[KMeans, DBSCAN]
+from gwpy.timeseries import TimeSeriesDict
+from gwpy.frequencyseries import FrequencySeries
 
-@dataclass
+import numpy as np
+
+from typing import Any
+
+
 class ClusteredSeismicData:
-    raw: TimeSeriesDict
-    blrms: TimeSeriesDict
-    clusters: SupportedClusterAlgorithms
+    '''
+    clusters data from a seismometer using a set of raw data streams and a set 
+    of bandlimited root-mean-squared (BLRMS) data streams
 
-    def vectors(self) -> np.ndarray:
-        return np.array(
+    Parameters
+    ----------
+    raw : TimeSeriesDict
+        set (e.g. x-, y-, and z-coordinates) of raw data from the seismometer
+    blrms : TimeSeriesDict
+        set (e.g. multiple frequency bands for each coordinate) of BLRMS data
+        from the seismometer
+    clustering : Any
+        clustering algorithm from scikit-learn to cluster the data
+
+    Attributes
+    ----------
+    raw : TimeSeriesDict
+        set (e.g. x-, y-, and z-coordinates) of raw data from the seismometer
+    blrms : TimeSeriesDict
+        set (e.g. multiple frequency bands for each coordinate) of BLRMS data
+        from the seismometer
+    clustering : Any
+        clustering algorithm from scikit-learn to cluster the data
+    vectors : np.ndarray of shape (number of samples, number of blrms time series)
+        vectorized blrms data
+    times : array-like of shape (number of samples)
+        list of gps-formatted times corresponding to each sample
+    labels : np.ndarry of shape (number of samples)
+        indicates the cluster that each vector belongs to
+    n_clusters : int
+        number of clusters
+    centers : np.ndarray of shape (n_clusters, number of blrms time series)
+        the average vector of each cluster
+    '''
+    def __init__(
+        self,
+        raw: TimeSeriesDict,
+        blrms: TimeSeriesDict,
+        clustering: Any
+    ):
+        self.raw = raw
+        self.blrms = blrms
+        self.clustering = clustering
+
+        # reduce self.blrms to vectors
+        self.vectors = np.array(
             list(self.blrms.values())
         ).T
 
-    def times(self) -> Index:
-        for ts in self.raw.values():
-            if hasattr(ts, 'times'):
-                return ts.times
+        try:
+            # get the times list from the times attribute of the first
+            #   TimeSeries in self.raw
+            self.times = next(iter(raw.values())).times
+        except StopIteration:
+            # all of the TimeSeries in self.raw should contain the times
+            #   attribute, so this should never be hit
+            assert False
 
-        # all of the TimeSeries in self.raw should contain the times
-        #   attribute, so this should never be hit
-        assert False
+        # fit the vector data using the specified clustering algorithm
+        self.clustering.fit(self.vectors)
+        
+        # get the resulting labels from the clustering
+        self.labels = self.clustering.labels_
 
-    def n_clusters(self) -> int:
-        if isinstance(self.clusters, KMeans):
-            return self.clusters.n_clusters
-        else:
-            return len({label for label in self.clusters.labels_ if label >=0})
+        # get the number of clustering based on the number of distinct labels
+        self.n_clusters = len({label for label in self.labels if label >= 0})
 
-    def centers(
-        self
-    ) -> np.ndarray:
-        '''
-        calculate the centers of each cluster
-    
-        Returns
-        -------
-        np.ndarray
-            the cluster centers, of shape (n_clusters, vector dimension)
-        '''
-        vectors = self.vectors()
-        labels = self.clusters.labels_
-        n_clusters = self.n_clusters()
-
-        # organize vectors by cluster label.
-        clusters = [np.empty(0) for _ in range(n_clusters)]
-        for i in range(len(vectors)):
-            if labels[i] not in range(n_clusters):
+        # organize vectors by cluster label
+        clusters = [[] for _ in range(self.n_clusters)]
+        for i in range(len(self.vectors)):
+            if self.labels[i] not in range(self.n_clusters):
                 # skip data that isn't in a cluster
                 continue
     
             # put the vector in its proper cluster
-            clusters[labels[i]] = np.append(clusters[labels[i]], [vectors[i]], axis=0)
-    
-        print(vectors)
-        print(vectors.shape)
-        for clust in clusters:
-            print(clust)
-            print(clust.shape)
-            print(np.mean(clust, axis=0))
+            clusters[self.labels[i]].append(self.vectors[i])
 
-        return np.array([ np.mean(clust, axis=0) for clust in clusters ])
-
+        # get the centers by reducing each cluster to its average vector
+        self.centers = np.array([
+            np.mean(np.array(clust), axis=0)
+            for clust in clusters
+        ])
 
     def closest_index(
         self,
@@ -83,15 +105,15 @@ class ClusteredSeismicData:
     
         Parameters
         ----------
-        vector : np.ndarray
-            a vector of shape (1, number of TimeSeries in self.blrms)
+        vector : np.ndarray of shape (1, number of blrms time series)
+            a vector
     
         Returns
         -------
         int
             index of closest vector in self.vectors to the passed vector
         '''
-        vectors = self.vectors()
+        vectors = self.vectors
         # assume to start that the closest index is the first vector in vectors
         closest_index = 0
 
@@ -101,6 +123,8 @@ class ClusteredSeismicData:
             ) < np.linalg.norm(
                 vector - vectors[closest_index]
             ):
+                # if vectors[i] is closer to vector than vectors[closest_index]
+                #   then update closest_index to i
                 closest_index = i
 
         return closest_index
@@ -109,27 +133,44 @@ class ClusteredSeismicData:
         self, 
         psd_length: float = 60,
     ) -> list[list[FrequencySeries]]:
-        times = self.times()
-        centers = self.centers()
-        print(centers)
-        print(centers.size)
+        '''
+        computes the power spectral density (PSD) of each center in self.centers
+
+        Parameters
+        ----------
+        psd_length : float, default=60
+            number of seconds from the time that the center occurred for which
+            to compute the psd
+        '''
+        times = self.times
+        centers = self.centers
         times_of_centers = [
+            # find the vector closest to center and use the corresponding time
+            #   for that vector
             times[self.closest_index(center)].value
             for center in centers
         ]
 
+        # copy the raw TimeSeriesDict n_clusters times (1 time for each center)
+        #   since crop overwrites the TimeSeriesDict
+        raw_copies = [self.raw.copy() for _ in range(self.n_clusters)]
+
         raw_cropped_to_centers = [
-            self.raw.crop(
+            # crop the raw data to a psd_length window around the point in
+            #   time that center occurred
+            raw_copy.crop(
                 start=times_of_centers[i],
                 end=times_of_centers[i] + psd_length,
                 copy=True
             )
-            for i in range(len(centers))
+            for i, raw_copy in enumerate(raw_copies)
         ]
-        for i in range(len(centers)):
-            print(centers[i], raw_cropped_to_centers[i].span)
 
         return [
-            [cropped_ts.psd() for cropped_ts in raw_cropped_to_center.values()] 
+            # compute the the psd for each cropped raw TimeSeries
+            [
+                cropped_ts.psd().crop(0.03, 30)
+                for cropped_ts in raw_cropped_to_center.values()
+            ] 
             for raw_cropped_to_center in raw_cropped_to_centers
         ]
